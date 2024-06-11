1db3aa5e9191912f854d4fe1f7a6cca3eb17770b
root <you@example.com>
2024-06-11 04:07:20 -0700

    Rotate 2D Matrix

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,30 +1,35 @@
 #!/usr/bin/python3
-"""Rotate a 2D matrix by 90 degrees"""
+"""2D matrix rotation module.
+"""
 
 
-def rotate_2d_matrix(matrix: list):
-    """Rotate a 2D matrix by 90 degrees"""
-    length = len(matrix[0])
-    matrix.extend([[] for _ in range(length)])
+def rotate_2d_matrix(matrix):
+    """Rotates an m by n 2D matrix in place.
 
-    for i in range(len(matrix), len(matrix) - length, -1):
-        for j in range(len(matrix) - length, 0, -1):
-            matrix[i - 1].append(matrix[j - 1].pop())
-    del matrix[0 : len(matrix) - length]
+    Parameters:
+    matrix (list of list of int): 2D matrix to rotate.
 
-
-# Pythonic way
-# def rotate_2d_matrix(matrix: list):
-#     matrix[:] = [list(row) for row in zip(*matrix[::-1])]
-# matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-
-# optimized way
-# def rotate_2d_matrix(matrix: list):
-#     n = len(matrix)
-#     for i in range(n // 2):
-#         for j in range(i, n - i - 1):
-#             temp = matrix[i][j]
-#             matrix[i][j] = matrix[n - 1 - j][i]
-#             matrix[n - 1 - j][i] = matrix[n - 1 - i][n - 1 - j]
-#             matrix[n - 1 - i][n - 1 - j] = matrix[j][n - 1 - i]
-#             matrix[j][n - 1 - i] = temp
+    Returns:
+    None
+    """
+    if type(matrix) != list:
+        return
+    if len(matrix) <= 0:
+        return
+    if not all(map(lambda x: type(x) == list, matrix)):
+        return
+    rows = len(matrix)
+    cols = len(matrix[0])
+    if not all(map(lambda x: len(x) == cols, matrix)):
+        return
+    c, r = 0, rows - 1
+    for i in range(cols * rows):
+        if i % rows == 0:
+            matrix.append([])
+        if r == -1:
+            r = rows - 1
+            c += 1
+        matrix[-1].append(matrix[r][c])
+        if c == cols - 1 and r >= -1:
+            matrix.pop(r)
+        r -= 1
